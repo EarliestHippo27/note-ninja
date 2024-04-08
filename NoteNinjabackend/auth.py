347ddfe9e534b29d0c7a4d24f45a92a5c5456120
johny <johny.tran2021@gmail.com>
2024-04-08 16:20:34 -0400

    Fixed many front-end HTML and styles.css issues. Fixed login and registering.

--- NoteNinjabackend/auth.py
+++ NoteNinjabackend/auth.py
@@ -32,11 +32,13 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    data = request.form
+    print(data)
     if request.method == 'POST':
         email = request.form.get('email')
         username = request.form.get('username')
-        password1 = request.form.get('password1')
-        #password2 = request.form.get('password2')
+        password1 = request.form.get('password')
+        password2 = request.form.get('confirm_password')
         
         user = User.query.filter_by(email=email).first()
         if user:
@@ -45,16 +47,16 @@ def sign_up():
             flash('Email must be more than 3 characters.', category='error')
         elif len(username) < 5:
             flash('Username must be more than 4 characters', category='error')    
-        #elif password1 != password2:
-           #flash('Passwords don\'t match.', category='error')    
+        elif password1 != password2:
+           flash('Passwords don\'t match.', category='error')    
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='sha256'))
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)
-            db.seesion.commit()
-            login_user(user, remember=True)
+            db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
                    
-    return render_template("")
+    return render_template("register.html")
